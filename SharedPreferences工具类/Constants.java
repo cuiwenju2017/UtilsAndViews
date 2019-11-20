38ec9061659f839d25ce118dc06be86982cc4b76
Administrator <1755140651@qq.com>
2019-11-20 13:06:24 +0800

    2019/11/20

---
+++ SharedPreferences工具类/Constants.java
@@ -0,0 +1,21 @@
+package com.mirkowu.intelligentelectrical.app;
+
+/**
+ * @author by DELL
+ * @date on 2018/2/6
+ * @describe
+ */
+
+public class Constants {
+    public static final String MD5_KEY = "123456";
+    public static final String  SP_KEY_USERINFO="userinfo";
+    public static final String LOGIN_STATUS ="login_status" ;
+    public static String device="device";
+    public static String power_name="power_name";
+
+    public static  String device_detail_direct_resource_page="device_detail_direct_resource";
+    public static final int resource_add_advice=1003;
+    public static final int resource_edit_advice=1004;
+    public static final int resource_device_detail=1005;
+    public static String parameter="parameter";
+}
