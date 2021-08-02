872d7506fda6d98f74996fe38400f47ff20553b1
Administrator <1755140651@qq.com>
2021-08-02 14:42:18 +0800

    2021/08/02

---
+++ app/src/main/java/com/example/utilsandviews/utils/FileUtils.java
@@ -0,0 +1,30 @@
+package com.example.utilsandviews.utils;
+
+/**
+ * 文件后缀名(不带点)与文件名获取
+ * eg:FileUtils.getExtensionName(file.getName())
+ */
+public class FileUtils {
+
+    //获取文件扩展名
+    public static String getExtensionName(String filename) {
+        if ((filename != null) && (filename.length() > 0)) {
+            int dot = filename.lastIndexOf('.');
+            if ((dot > -1) && (dot < (filename.length() - 1))) {
+                return filename.substring(dot + 1);
+            }
+        }
+        return filename;
+    }
+
+    //获取不带扩展名的文件名
+    public static String getFileNameNoEx(String filename) {
+        if ((filename != null) && (filename.length() > 0)) {
+            int dot = filename.lastIndexOf('.');
+            if ((dot > -1) && (dot < (filename.length()))) {
+                return filename.substring(0, dot);
+            }
+        }
+        return filename;
+    }
+}
--- app/src/main/java/com/example/utilsandviews/utils/OneClickThree.java
+++ app/src/main/java/com/example/utilsandviews/utils/OneClickThree.java
@@ -3,7 +3,7 @@ package com.example.utilsandviews.utils;
 public class OneClickThree {
 
     // 两次点击间隔不能少于n秒 true:点击间隔小于n秒 false:点击间隔大于n秒
-    private static final int FAST_CLICK_DELAY_TIME = 1000 * 3;
+    private static final int FAST_CLICK_DELAY_TIME = 1000 * 1;
     private static long lastClickTime;
 
     public static boolean isFastClick() {
